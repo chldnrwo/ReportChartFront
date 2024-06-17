ff21c8568bee4be9b4250f92b57b84b0e11e6baf
chldnrwo <chldnrwo777@naver.com>
2024-06-17 17:48:04 +0900

    차트 서버 성능 개선, x축 y축 개선

--- chartGenerator.js
+++ chartGenerator.js
@@ -1,16 +1,17 @@
 const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
 const fs = require('fs').promises;
 const path = require('path');
+const moment = require('moment');
 
-const width = 1000; //px
-const height = 520; //px
+const width = 1000; // px
+const height = 520; // px
 const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height });
 
 async function generateChart(data) {
   const configuration = {
     type: 'line',
     data: {
-      labels: data.data.map(d => d.column1),
+      labels: data.data.map(d => moment(d.column1)), // moment.js를 사용하여 날짜를 포맷합니다.
       datasets: [{
         label: data.title,
         data: data.data.map(d => d.column2),
@@ -25,19 +26,34 @@ async function generateChart(data) {
       responsive: true,
       maintainAspectRatio: true,
       scales: {
-        x: {
+        xAxes: [{
           type: 'time',
           time: {
             unit: 'day',
+            tooltipFormat: 'll',
             displayFormats: {
-              day: 'MMM d'
+              day: 'MMM D'
             }
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Date'
           }
-        },
-        y: {
-          min: 0,
-          max: 100
-        }
+        }],
+        yAxes: [{
+          ticks: {
+            min: 0,
+            max: 100
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Percentage'
+          }
+        }]
+      },
+      legend: {
+        display: true,
+        position: 'top'
       }
     }
   };
